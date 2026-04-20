db6cf92af15858af07185186fe50bb53ad15bc2d
SaraNaqi <stn5144@psu.edu>
2026-04-20 00:25:21 -0400

    final check in 1 update

--- swing-champions-arena/swing-champions-player-card.js
+++ swing-champions-arena/swing-champions-player-card.js
@@ -3,6 +3,7 @@ export class SwingChampionsPlayerCard extends LitElement {
     static get tag() {
         return "swing-champions-player-card";
     }
+    //
     render() {
         return html`
         <p> player cards info </p>
